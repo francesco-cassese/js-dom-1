dfc510eca3423afb9318e3098c888f3661f5d65a
Francesco Cassese <francescocassese9@gmail.com>
2026-04-02 15:45:58 +0200

    Fix: tag nella struttura html e collegati i vari elementi in js

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// -- Collego gli elementi html ---
+
+const immagineLampadina = document.querySelector('.container-img');
+const interruttoreLuce = document.querySelector('#switch');
+const statoInterruttore = document.querySelector('.title-main');
+const commentoInterruttore = document.querySelector('.txt-main');
