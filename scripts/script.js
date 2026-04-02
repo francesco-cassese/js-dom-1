ff00048b7536cec36dee34e62518decbc00d5a72
Francesco Cassese <francescocassese9@gmail.com>
2026-04-02 16:53:01 +0200

    Aggiunti testi dinamici

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ const immagineLampadina = document.querySelector('.container-img');
 const interruttoreLuce = document.querySelector('#switch');
 const statoInterruttore = document.querySelector('.title-main');
 const commentoInterruttore = document.querySelector('.txt-main');
+const frasiDinamiche = document.querySelector('.text-main');
 const wrapper = document.querySelector('.wrapper');
 
 // --- Variabili ---
@@ -21,14 +22,18 @@ function accendiLampadina() {
         immagineLampadina.innerHTML = lampadinaAccesa;
         luceAccesa = true;
         statoInterruttore.innerHTML = ('<h3 class="title-main">Interruttore ACCESO</h3>');
+        commentoInterruttore.innerHTML = ('<p class="txt-main"> HAI VISTO? Adesso ricordati di spegnerla prima di andare via</p>');
         wrapper.style.backgroundColor = 'white';
+        frasiDinamiche.style.color = 'black';
     } else {
 
         //SPEGNI
         immagineLampadina.innerHTML = lampadinaSpenta;
         luceAccesa = false;
         statoInterruttore.innerHTML = ('<h3 class="title-main">Interruttore SPENTO</h3>');
+        commentoInterruttore.innerHTML = ('<p class="txt-main">Clicca qui e guarda la magia</p>');
         wrapper.style.backgroundColor = 'black';
+        frasiDinamiche.style.color = 'white';
     }
 }
 
